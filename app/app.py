51fe275931ba991d6731c9301e0dd4f393ea8401
haiqalhasly <haiqalhasly@gmail.com>
2025-08-13 21:49:26 +0800

    Initialize app with JSON data structure and command-line interface for marking deeds as done

--- app/app.py
+++ app/app.py
@@ -1,2 +1,22 @@
+import argparse
+import json
+import os
 
-print("Hello world")
+# Create JSON file if it doesn't exist
+if not os.path.exists("data.json"):
+    with open("data.json", "w") as file:
+        json.dump([], file)
+
+# Open the file
+with open("data.json", "r") as file :
+    data = json.load(file)
+
+
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers(dest="command", help="command for subparse")
+
+# Mark as done parser
+mark_done_parser = subparser.add_parser("mark-done", help="mark as done parser")
+mark_done_parser.add_argument("deed_id", help="add your deeds id", type=int)
+
