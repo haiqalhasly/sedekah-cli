9573e7af88d96cabb1b921fd68df90063088b10c
haiqalhasly <haiqalhasly@gmail.com>
2025-08-14 11:34:58 +0800

    Add player management and update status for deeds in app.py and data.json

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ if not os.path.exists("data.json"):
 with open("data.json", "r") as file :
     data = json.load(file)
 
+# Create JSON file if it doesn't exist
+if not os.path.exists("player.json"):
+    with open("player.json", "w") as file:
+        json.dump([], file)
+
+# Open the file
+with open("player.json", "r") as file :
+    player_data = json.load(file)
+
 
 
 parser = argparse.ArgumentParser()
@@ -23,28 +32,39 @@ mark_done_parser.add_argument("deed_id", help="add your deeds id", type=int)
 args = parser.parse_args()
 
 
-#function to load data
+#function to load data.json
 def load_data(data):
     with open("data.json", "w") as file :
         json.dump(data, file, indent=4)
         
-
-def mark_done(data, deed_id):
+#function to load player.json
+def load_coin(player_data):
+    with open("player.json", "w") as file :
+        json.dump(player_data, file, indent=4)
+    
+def mark_done(data, deed_id,player_data):
     deeds = data["deeds"]
     deed_found = False
+
+    players = player_data["player"]
+    player_one = players[1]
 
     for i,deed in enumerate(deeds):
         if deed["id"] == deed_id:
             deed["status"] = "done"
             load_data(data)
             deed_found = True
+
         
 
     if deed_found == True:
         print(f"Deed (ID:{deed_id}) marked")
+        player_one["coin"] +=1
+        load_coin(player_data)
+
     else:
         print(f"Deed (ID:{deed_id}) not found")
 
 if args.command == "mark-done":
     deed_id = args.deed_id
-    mark_done(data,deed_id)
+    mark_done(data,deed_id,player_data)
